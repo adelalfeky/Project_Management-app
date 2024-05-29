506b6f973230591a7bb44077e4641e615caf35d1
Adel Ali <adel1@outlook.com>
2024-05-29 17:14:56 +0300

    update _ main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientManagementComponent } from './client-management/client-management.component';
+import { HomeComponent } from './home/home.component';
+import { MeetingManagementComponent } from './meeting-management/meeting-management.component';
+import { MeetingListComponent } from './meeting-list/meeting-list.component';
+import { ClientListComponent } from './client-list/client-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientManagementComponent,
+    HomeComponent,
+    MeetingManagementComponent,
+    MeetingListComponent,
+    ClientListComponent
   ],
   imports: [
     BrowserModule,
